7a553c4db990131d6563c208eb02e043d943cbf8
Philippe Gris <gris@clermont.in2p3.fr>
2022-05-18 13:59:15 +0200

    add a new class to save data on the fly

--- ztf_pipeutils/ztf_hdf5.py
+++ ztf_pipeutils/ztf_hdf5.py
@@ -11,6 +11,97 @@ import pandas as pd
 import os
 
 
+class Write_LightCurve_cumul:
+    "Definition of a class that records light curves and/or their meta data in an hdf5 file"
+
+    def __init__(self, outputDir='dataLC', file_data='Data.hdf5', file_meta='Meta.hdf5', path_prefix='SN', List=['z', 't0',
+                                                                                                                 'x0', 'x1', 'c', 'mwebv', 'ra', 'dec', 'mwebv_sfd98', 'idx_orig'], **kwargs):
+        """
+        Parameters
+        --------------
+        outputDir: str, opt
+          output directory (default: dataLC)
+        file_data : str, opt
+            Name of the data file that you want to write (default='Data.hdf5').
+        file_meta : str, opt
+            Name of the meta data file that you want to write (default='Meta.hdf5').
+        path_prefix: str, opt
+          prefix for the path in hdf5 files (default: SN)
+        """
+
+        self.List = List
+
+        self.outputDir = outputDir
+        self.path_prefix = path_prefix
+
+        self.data_name = file_data
+        self.meta_file = os.path.join(self.outputDir, file_meta)
+        self.data_file = os.path.join(self.outputDir, file_data)
+
+        if not os.path.exists(self.outputDir):
+            os.makedirs(self.outputDir)
+        if os.path.isfile(self.data_file):
+            os.remove(self.data_file)
+        if os.path.isfile(self.meta_file):
+            os.remove(self.meta_file)
+
+        self.file_data = h5py.File('{}'.format(self.data_file), 'w')
+        #self.file_meta = h5py.File('{}'.format(self.meta_file), 'w')
+
+        self.metaData = Table()
+
+    def write(self, lc, metaSN, metaRej, path='SN', pathRej='Rej', serialize_meta=True):
+        """
+        method to save light curves and metadata
+
+        Parameters
+        --------------
+        lc: 
+          SN light curves
+        metaSN: 
+          metadata corresponding to LC
+        metaRej: 
+          metadata corresponding to rejected simu
+
+        """
+        for j, llc in enumerate(lc):
+            if llc is not None:
+                if llc.meta is not None:
+                    # update metadata with path
+                    mmeta = metaSN[j]
+                    thepath = '{}_{}'.format(path, j)
+                    metadict = {name: mmeta[name]
+                                for name in mmeta.dtype.names}
+                    metadict['path'] = thepath
+                    # update total metaData
+                    t = Table([metadict])
+                    self.metaData = vstack(
+                        [self.metaData, t])
+                    # write lc
+                    llc.meta = metadict
+                    astropy.io.misc.hdf5.write_table_hdf5(
+                        llc, self.file_data, path=thepath, overwrite=True, serialize_meta=serialize_meta)
+
+        if metaRej is not None:
+            meta_rej = Table(metaRej)
+            pp = ['{}_{}'.format(pathRej, i)
+                  for i in range(len(meta_rej))]
+            col = Column(pp, name='path')  # shape=(2,)
+            meta_rej.add_column(col)
+            self.metaData = vstack([self.metaData, meta_rej])
+
+        self.metaData.meta["directory"] = self.outputDir
+        self.metaData.meta["file_name"] = self.data_name
+        #print('meta data', self.metaData.meta, self.metaData)
+
+        if os.path.isfile(self.meta_file):
+            os.remove(self.meta_file)
+        file_meta = h5py.File('{}'.format(self.meta_file), 'w')
+
+        astropy.io.misc.hdf5.write_table_hdf5(
+            self.metaData, file_meta, path='meta', overwrite=True, serialize_meta=serialize_meta)
+
+
 class Write_LightCurve:
     "Definition of a class that records light curves and/or their meta data in an hdf5 file"
 
